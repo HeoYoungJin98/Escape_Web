e7d3a52fb37e5668600a801f282aae6dbd5adaa9
HeoYooungJin98 <Youngjin.heo98@gmail.com>
2021-09-28 21:50:04 +0900

    Add Room_Back actions

--- src/Stage_1_Mirror/Room_Mirror.js
+++ src/Stage_1_Mirror/Room_Mirror.js
@@ -1,6 +1,12 @@
 document.getElementById("face").addEventListener("click",Face_pop)
 function Face_pop(){
-    window.open("../img/Room_Mirror/Room_Mirror_Face.jpeg","pop",
+    window.open("../../img/Room_Mirror/Room_Mirror_Face.jpeg","pop",
+        "width=400,height=500,history=no,resizable=no,status=no,scrollbars=no,menubar=no,left=500,top=150");
+}
+
+document.getElementById("View_Lock_1").addEventListener("click",Lock_pop)
+function Lock_pop(){
+    window.open("Room_Mirror_Lock.html","pop",
         "width=400,height=500,history=no,resizable=no,status=no,scrollbars=no,menubar=no,left=500,top=150");
 }
 
@@ -51,4 +57,22 @@ function View_Back(){
     document.getElementById("Left_Wall").style.visibility = "hidden";//왼쪽 비활성화
     document.getElementById("Right_Wall").style.visibility = "hidden";//우측 활성화
     document.getElementById("Back_Wall").style.visibility = "visible";//후면 비활성화
+}
+
+let hammer = 1;
+let State = 0;
+document.getElementById("Destroy").addEventListener("click",destroy);
+function destroy(){
+    if(hammer === 1 && State === 0){
+        document.getElementById("Back_Wall_IMG").src = "../../img/Room_Mirror/Room_Mirror_back_destroying.jpeg";
+        State = 1;
+    }
+    else if(hammer === 1 && State === 1){
+        document.getElementById("Back_Wall_IMG").src = "../../img/Room_Mirror/Room_Mirror_back_destroyed.jpeg";
+        State = 2;
+    }
+    else if(State === 2){
+        window.open("../../img/Room_Mirror/Room_Mirror_liver.jpeg","pop",
+            "width=400,height=500,history=no,resizable=no,status=no,scrollbars=no,menubar=no,left=500,top=150");
+    }
 }
